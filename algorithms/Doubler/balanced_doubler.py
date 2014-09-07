fixed average of averages bug

algorithms/Doubler/balanced_doubler.py
@@ -45,21 +45,22 @@ def run_doubler_algorithm(means, log_horizon):
         cumulative regret. """
 
     # The L set (initialized to [1,0,0,...,0])
-    my_left_set = [False]*len(means)
-    my_left_set[0] = 1
+    my_left_set = [True]*len(means)
+
+    hot_start_plays = 7
 
     # The number of arms
     n_arms = len(means)
 
     # Shuffling the means vector.
-    random.shuffle(means)
+    #random.shuffle(means)
 
     # Assigning Bernoulli arms
     arms = map(lambda (mu): BernoulliArm(mu), means)
 
     # Assigning the black-boxes with the UCB 3 algorithm
-    #left_black_box = UCB3(counts=[], p_counts=log_horizon, values=[], alpha=0.5)
-    right_black_box = UCB3(counts=[], values=[], alpha=0.75)
+    right_black_box = UCB3(counts=[], values=[], log_horizon=log_horizon, values_in_epoch=[], plays_in_epoch=[],
+                           alpha=0.25, hot_start_plays=hot_start_plays)
 
     # Initializing the black-boxes.
     right_black_box.initialize(n_arms)
@@ -76,18 +77,14 @@ def run_doubler_algorithm(means, log_horizon):
     # The record of all the arms used in each epoch
     arms_used = np.zeros([n_arms, log_horizon+1])
 
-    # The Doubler algorithm :
-    for current_p in range(1, log_horizon+1):
+    # The algorithm :
+    for current_p in range(6, log_horizon+1):
 
         # The arms used in this current round
         arms_histogram = [0]*n_arms
 
         # This round time interval.
         current_time_interval = time_interval(current_p)
-
-        # Initializing the right black-box (S).
-
-        right_black_box.initialize(n_arms)
 
         for t in current_time_interval:
 
@@ -99,10 +96,7 @@ def run_doubler_algorithm(means, log_horizon):
 
             # Choosing an arm using the right black box.
             # (I also take in consideration the arms used in the current epoch)
-            right_arm = right_black_box.select_arm(current_p, t, arms_used)
-
-            # Updating the record of used arms
-            arms_used[right_arm, current_p] += 1
+            right_arm = right_black_box.select_arm(current_p, t)
 
             # Updating the histogram of the current epoch
             arms_histogram[right_arm] += 1
@@ -116,12 +110,12 @@ def run_doubler_algorithm(means, log_horizon):
             observed_b[t] = observe_b_t(current_left_reward, current_right_reward)
 
             # Updating the right black-box with b_t and the bonus
-            right_black_box.update(right_arm, observed_b[t]+right_black_box.calculate_bonus(current_p, t, arms_used, right_arm))
+            right_black_box.update(right_arm, observed_b[t], current_p)
 
             # Assigning the average reward.
             average_reward[t] = float(current_left_reward + current_right_reward) / 2
 
-            print "arms: {0}, {1}".format(left_arm, right_arm)
+            #print "arms: {0}, {1}".format(left_arm, right_arm)
             # Assigning the regret
             regret[t] = max(means) - average_reward[t]
 

algorithms/ucb/ucb3.py
@@ -10,31 +10,38 @@ def ind_max(x):
 
 
 class UCB3():
-    def __init__(self, counts, values, alpha=0.5):
+    def __init__(self, counts, values, log_horizon, values_in_epoch, plays_in_epoch, alpha=0.5, hot_start_plays=5):
         self.counts = counts
         self.values = values
         self.alpha = alpha
+        self.log_horizon = log_horizon
+        self.values_in_epoch = values_in_epoch
+        self.plays_in_epoch = plays_in_epoch
+        self.hot_start_plays = hot_start_plays
         return
 
     def initialize(self, n_arms):
         self.counts = [0 for col in range(n_arms)]
+
         self.values = [0.0 for col in range(n_arms)]
+        self.values_in_epoch = np.zeros([n_arms, self.log_horizon+1])
+        self.plays_in_epoch = np.zeros([n_arms, self.log_horizon+1])
         return
 
-    def calculate_bonus(self, p, t, arms_used, arm):
+    def calculate_bonus(self, p, t, arm):
         inverse_sum = 0
-        for n_i in arms_used[arm, :]:
+        for n_i in self.plays_in_epoch[arm, :]:
             if n_i > 0:
                 inverse_sum += 1/(n_i+.0)
 
         return math.sqrt(self.alpha*math.log(t)*inverse_sum/(p**2+.0))
 
-    def select_arm(self, p, t, arms_used):
+    def select_arm(self, p, t):
 
         n_arms = len(self.counts)
 
         for arm in range(n_arms):
-            if self.counts[arm] < 1:
+            if self.plays_in_epoch[arm, p] < self.hot_start_plays:
 
             # if sum(arms_used[arm, :]) < 1:
                 return arm
@@ -44,22 +51,29 @@ class UCB3():
 
         for arm in range(n_arms):
 
-            bonus[arm] = self.calculate_bonus(p, t, arms_used, arm)
+            bonus[arm] = self.calculate_bonus(p, t, arm)
 
             ucb_values[arm] = self.values[arm] + bonus[arm]
 
         return ind_max(ucb_values)
 
-    def update(self, chosen_arm, reward):
+    def update(self, chosen_arm, reward, p):
 
-        self.counts[chosen_arm] += 1
+        self.plays_in_epoch[chosen_arm, p] += 1
+        self.values_in_epoch[chosen_arm, p] += reward
 
-        n = self.counts[chosen_arm]
-
-        value = self.values[chosen_arm]
-
-        new_value = ((n - 1) / float(n)) * value + (1 / float(n)) * reward
+        new_value = self.calculate_value(chosen_arm, p)
 
         self.values[chosen_arm] = new_value
 
         return
+
+    def calculate_value(self, arm, p):
+
+        average_of_averages = 0
+
+        for i in range(p+1):
+            if self.plays_in_epoch[arm, i] != 0:
+                average_of_averages += (self.values_in_epoch[arm, i] / (self.plays_in_epoch[arm, i]+.0))/(p+.0)
+
+        return average_of_averages